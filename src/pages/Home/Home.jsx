a3d3005f639c4e149e2f76a89adb253f43f2bc7a
MartaShchur <m.matseyko199@gmail.com>
2024-02-22 15:12:14 +0100

    add fix

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,13 @@
-import { useEffect, useState } from 'react';
-
-import { getTrendingMovies } from 'services/TmbdApi';
+import React from "react";
 
 
 const Home = () => {
-  const [cars, setCars] = useState([]);
-
-  useEffect(() => {
-    getTrendingMovies('').then(setCars);
-  }, []);
-
   return (
     <>
       <h1> Car rental in Ukraine and Europe </h1>
       
       <p>In the busy rhythm of modern life, a personal car is the best means of transport for people who value their time and want to get things done on time. It provides mobility, independence and freedom of movement with maximum comfort. If you don't have your own car or you can't use it due to breakdowns after arriving in another city by train or plane, renting a car will help you. Today, such a service is becoming more and more popular in Ukraine, where there are already 500 rental points operating in different cities. Our service will help you rent a car of the right class, make and model in any city of our country and beyond.</p>
-      {/* <FilmsList movies={cars} /> */}
+      
     </>
   );
 };
